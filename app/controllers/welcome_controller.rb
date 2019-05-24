b6d8bf084fb3db55aba25945c2dfbd35fd9cd51a
Pavel Smolov <palexsmol@gmail.com>
2019-05-23 21:29:55 -0700

    add username field to users tab & upd devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
     def firstpage
-    
+        # if user_signed_in?
+        #     redirect_to posts_path
+        # else
+        #     redirect_to new_user_session_path
+        # end
     end
     
     def about
